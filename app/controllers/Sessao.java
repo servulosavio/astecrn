1e1f80d607dcf6332dbfe22575ed6f50350729b5
Sérvulo Sávio <servulosavio@gmail.com>
2018-06-17 00:08:29 -0300

    Tela de login e mensagem de erro

--- app/controllers/Sessao.java
+++ app/controllers/Sessao.java
@@ -8,8 +8,8 @@ public class Sessao extends Controller {
 	public static void login(String login, String senha) {
 		Usuario usuario = Usuario.find("login = ? and senha = ?", login, senha).first();
 		if(usuario == null) {
-			flash.error("Login ou Senha inválidos");
-			Application.index();
+			flash.error("Usuário ou Senha inválidos, favor tente novamente");
+			Application.login();
 		} else {
 			Application.gerenciador();
 		}
